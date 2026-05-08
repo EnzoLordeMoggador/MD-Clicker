aacde03f5e06d718351e552c5508e0d53e67a520
= <=>
2026-05-08 09:36:29 -0300

    Atualizações

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let pontos = document.getElementById("pontos");
 let buttonclicker = document.getElementById("buttonclicker")
-let G = 0
+let G = null
 
 buttonclicker.addEventListener("click", () => {
     let valor = parseInt(pontos.innerText);
     pontos.innerText = valor+1
+    if (G === null) {
+        G = 0;
+        Cl();
+    }
 })
 
-while (true) {
-   buttonclicker.style.transform = "rotate(Gdeg)";
+function Cl() {
+        G += 1
+        buttonclicker.style.transform = `rotate(${G}deg)`;
+        requestAnimationFrame(Cl);
 }
