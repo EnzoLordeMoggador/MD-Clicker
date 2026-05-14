eb00111bb40731e22bd4d01832420b7edee45d5c
= <=>
2026-05-14 12:09:49 -0300

    Update Three

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+let emailL = document.getElementById("emailL");
+let senhaL = document.getElementById("senhaL");
 let pontos = document.getElementById("pontos");
 let buttonclicker = document.getElementById("buttonclicker");
 let PlayButton = document.getElementById("Login");
 let PIDiv = document.querySelector(".PaginaInicial");
 let DivGame = document.querySelector(".GameDiv");
-let Cadastrar = document.querySelector(".Cadastrar")
+let Cadastrese = document.getElementById("Cadastre-se");
+let CadastrarDiv = document.getElementById("CadastrarDiv");
+let Cadastrar = document.getElementById("Cadastrar")
+let LoginDiv = document.getElementById("LoginDiv");
 let G = null;
 
 buttonclicker.addEventListener("click", () => {
@@ -21,6 +26,19 @@ function Cl() {
         requestAnimationFrame(Cl);
 }
 PlayButton.addEventListener("click", () => {
+    
+    if (emailL.value == "" || senhaL.value == ""){
+      alert("Coloque os Dados Do Login")
+      return;
+    }
+
     PIDiv.style.display = "none"
     DivGame.style.display = "flex"
+})
+Cadastrese.addEventListener("click", () => {
+    PlayButton.style.display = "none";
+    Cadastrese.style.display = "none";
+    Cadastrar.style.display = "flex";
+    LoginDiv.style.display = "none";
+    CadastrarDiv.style.display = "inline-flex";
 })
